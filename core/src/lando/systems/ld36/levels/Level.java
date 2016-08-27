c376c1e8607c3d5e228f9d8581090c6a5a0319bf
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2016-08-27 16:56:40 -0500

    Spawning in map, sorting game objects by y position for correctly layered rendering

--- core/src/lando/systems/ld36/levels/Level.java
+++ core/src/lando/systems/ld36/levels/Level.java
@@ -4,12 +4,17 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Sort;
+import lando.systems.ld36.entities.GameObject;
 import lando.systems.ld36.entities.Player;
 import lando.systems.ld36.utils.Assets;
+
+import java.util.Comparator;
 
 /**
  * Created by Brian on 8/27/2016.
@@ -17,11 +22,21 @@ import lando.systems.ld36.utils.Assets;
 public class Level {
 
     public TiledMap map;
+    public Array<GameObject> objects;
     TiledMapRenderer renderer;
     TiledMapTileLayer groundLayer;
     Array<TiledMapImageLayer> imageLayers;
 
     Player player;
+
+    Comparator<GameObject> gameObjectYPosComparator = new Comparator<GameObject>() {
+        @Override
+        public int compare(GameObject o1, GameObject o2) {
+            if      (o1.position.y == o2.position.y) return 0;
+            else if (o1.position.y >  o2.position.y) return -1;
+            else                                     return 1;
+        }
+    };
 
     public Level(String name) {
         load(name, Assets.batch);
@@ -29,10 +44,16 @@ public class Level {
 
     public void setPlayer(Player player) {
         this.player = player;
+        objects.add(this.player);
     }
 
     public void update(float dt) {
+        for (GameObject object : objects) {
+            object.update(dt);
+        }
         player.update(dt);
+
+        Sort.instance().sort(objects, gameObjectYPosComparator);
     }
 
     public void render(SpriteBatch batch, OrthographicCamera camera) {
@@ -42,14 +63,16 @@ public class Level {
         }
         renderer.renderTileLayer(groundLayer);
 
-        player.render(batch);
+        for (GameObject object : objects) {
+            object.render(batch);
+        }
     }
 
     private void load(String mapName, SpriteBatch batch){
         final TmxMapLoader mapLoader = new TmxMapLoader();
 
         map = mapLoader.load(mapName);
-//        loadMapObjects();
+        loadMapObjects();
 
         renderer = new OrthogonalTiledMapRenderer(map, 1f, batch);
 
@@ -65,6 +88,34 @@ public class Level {
                 imageLayer.setY(imageLayer.getY() + Float.parseFloat(props.get("offset_y", "0", String.class)));
                 imageLayers.add(imageLayer);
             }
+        }
+    }
+
+    private void loadMapObjects() {
+        if (map == null) return;
+
+        objects = new Array<GameObject>();
+
+        MapProperties props;
+        MapLayer objectLayer = map.getLayers().get("objects");
+        for (MapObject object : objectLayer.getObjects()) {
+            props = object.getProperties();
+            float w = (Float) props.get("width");
+            float h = (Float) props.get("height");
+            float x = (Float) props.get("x");
+            float y = (Float) props.get("y");
+            String type = (String) props.get("type");
+
+            // Instantiate based on type
+            if (type.equals("player")) {
+                Player p = new Player();
+                p.position.x = x;
+                p.position.y = y;
+                objects.add(p);
+            }
+//            else if (type.equals("...")) {
+//                new GameObject(this, x / 16, (y / 16) + 1, false);
+//            }
         }
     }
 
