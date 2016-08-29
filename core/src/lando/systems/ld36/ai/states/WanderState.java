07a103a996afd71d406a65b39cf5906e53768e9f
Doug Graham <dsgraham@gmail.com>
2016-08-28 20:30:47 -0500

    Keep wanders on the screen.

--- core/src/lando/systems/ld36/ai/states/WanderState.java
+++ core/src/lando/systems/ld36/ai/states/WanderState.java
@@ -19,13 +19,15 @@ public class WanderState extends State{
         float moveLeft = owner.moveSpeed * dt;
         while (moveLeft > 0) {
             if (owner.movePoint.epsilonEquals(0, 0, .5f)) {
-                float x = owner.position.x + MathUtils.random(200) - 100;
+                float x = owner.position.x + MathUtils.random(500) - 250;
                 float y = owner.position.y + MathUtils.random(200) - 100;
                 y = MathUtils.clamp(y, owner.bottomPlayArea, owner.topPlayArea);
                 x = MathUtils.clamp(x, owner.leftEdge, owner.level.getLevelWidth() - (owner.width / 2));
                 owner.movePoint.set(x, y);
             }
-
+            owner.movePoint.x = MathUtils.clamp(owner.movePoint.x,
+                                                owner.level.screen.cameraCenter.x - owner.level.screen.camera.viewportWidth/2,
+                                                owner.level.screen.cameraCenter.x + owner.level.screen.camera.viewportWidth/2);
             moveLeft = owner.updateMove(dt, moveLeft);
         }
     }
